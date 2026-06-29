10a55962d7dae5d41214a5c5a0a8c6f1cbb98d34
Jannat Garg <jannatgarg2005@gmail.com>
2026-06-29 23:16:04 +0530

    Day 1: Variables and Data Types

---
+++ 01-basics/class1_variables_datatypes.py
@@ -0,0 +1,21 @@
+# First program 
+
+from os import name
+
+
+print("Hello, World!")
+
+print(23+3)
+print(23-3)
+print(23*3)
+print(23/3)
+print(23//3)
+print(23%3)
+
+# Variable is a container that holds data in memory.
+x = 10
+print(x)
+c = 20
+print(c)
+name = "Jannat"
+print("my name is", name)
